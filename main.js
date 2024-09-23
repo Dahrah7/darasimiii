8d6e82fdf51ae14a7289219ff610408b2f994340
Dahrah7 <mobolajioluwadarasimi45@gmail.com>
2024-09-23 04:43:59 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+function show(){
+    document.getElementById("key").innerHTML ="My name is alausa mobolaji oluwadarasimi <br> I am a web developer<br>I am also a full stac developer<br> with three years experience press (x) to see less";
+}
+function hide(){
+    document.getElementById("key").innerHTML ="";
+}
